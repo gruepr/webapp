d7ec5aa33fcbb1c465ded9915cfc78846416e8b6
caocynthia <cynthiacao.work@gmail.com>
2022-11-14 14:04:40 -0500

    edit header colors

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,18 +6,17 @@ const Header = () => {
   return (
     <div className="flow-root items-center bg-white px-16 py-1">
       <div className="float-left flex items-center h-20 space-x-10">
-        <div className="text-2xl pb-2 paytone text-gblue">gruepr</div>
-        <div className="text-base font-bold dmsans text-dark space-x-4">
+        <div className="text-2xl pb-2 paytone text-primary-500">gruepr</div>
+        <div className="text-base font-bold dmsans text-neutral-500 space-x-4">
           <Link to="/">Home</Link>
           <Link to="/Faqs">FAQs</Link>
-
         </div>
       </div>
       <div className="float-right flex items-center justify-center h-20 space-x-8">
-        <div className="text-base font-normal dmsans text-dark">
+        <div className="text-base font-normal dmsans text-neutral-500">
           <a onClick={() => console.log("Home")}>info@gruepr.com</a>
         </div>
-        <button className="bg-dark py-4 px-8 text-base font-bold dmsans text-white">
+        <button className="bg-neutral-500 py-4 px-8 text-base font-bold dmsans text-white">
           <a onClick={() => console.log("FAQs")}>Download</a>
         </button>
       </div>
