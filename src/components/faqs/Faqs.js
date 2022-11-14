b90d5dc70a4e26da12ea51c015b5298707055b73
caocynthia <cynthiacao.work@gmail.com>
2022-11-14 13:08:47 -0500

    create footer

--- src/components/faqs/Faqs.js
+++ src/components/faqs/Faqs.js
@@ -1,10 +1,12 @@
 import Header from "../header/Header";
+import Footer from "../footer/Footer.js";
 
 const Faqs = () => {
   return (
     <div>
       <Header />
       <h1> FAQ section!!!!!</h1>
+      <Footer />
     </div>
   );
 };
