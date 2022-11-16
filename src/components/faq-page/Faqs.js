ae7355e856bae789b95175114f2114ea74266ccf
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-16 12:31:08 -0500

    heading for yt embd

--- src/components/faq-page/Faqs.js
+++ src/components/faq-page/Faqs.js
@@ -9,7 +9,7 @@ const Faqs = () => {
       <Header />
       <div class="bg-faq-bg">
         <h1 class=" px-20 py-20 text-3xl text-dark font-bold drop-shadow-lg dmsans ">How Gruepr works</h1>
-
+        
         <Youtube/>
         
         <Questions/>
--- src/components/faq-page/YouTube.js
+++ src/components/faq-page/YouTube.js
@@ -4,7 +4,7 @@ export const YouTube = () => {
     return (
 
         <div className="px-20">
-        
+             <h1 class="py-5 text-1xl text-dark font-bold drop-shadow-lg dmsans ">Full App Overview</h1>
             <iframe width="300" height="200" src="https://www.youtube.com/embed/Ja-PF068tII" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
         </div>
