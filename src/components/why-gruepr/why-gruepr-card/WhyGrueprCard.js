ce0d4b5c7cc2a38b3d569762ad9a8b3644cb0b70
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-16 20:05:50 -0500

    yellow cards

--- src/components/why-gruepr/why-gruepr-card/WhyGrueprCard.js
+++ src/components/why-gruepr/why-gruepr-card/WhyGrueprCard.js
@@ -6,7 +6,7 @@ import Card from './constants'
 function WhyGrueprCards(props) {
   return (
     <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      <div class=" bg-white rounded overflow-hidden shadow-lg">
+      <div class=" bg-accent-50  rounded overflow-hidden shadow-lg">
           <img class="object-none object-center m-auto" src={props.img} alt="icon" />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2 dmsans dark">{props.header}</div>
