2f009cef19839989c8e84e5f6e9e7bcd1d1b2a18
caocynthia <cynthiacao.work@gmail.com>
2022-11-18 16:08:52 -0500

    add content

--- src/components/why-gruepr/why-gruepr-card/constants.js
+++ src/components/why-gruepr/why-gruepr-card/constants.js
@@ -1,31 +1,22 @@
-import clipboard from '../../../img/clipboard.svg'
-import glasses from '../../../img/glasses.svg'
-import security from '../../../img/security.svg'
-
+import clipboard from "../../../img/clipboard.svg";
+import glasses from "../../../img/glasses.svg";
+import security from "../../../img/security.svg";
 
 export const CARD = [
-    {
-        img: clipboard,
-        header: "gruepr is flexible",
-        info: "Instructors can include whatever questions they would like to use to help form teams, and the algorithm can use the survey data in a variety of instructor-chosen ways."
-        
-    },
-    {
-        img: security,
-        header: "gruepr keeps your data private",
-        info: "Instructors can include whatever questions they would like to use to help form teams, and the algorithm can use the survey data in a variety of instructor-chosen ways."
-        
-       
-    },
+  {
+    img: clipboard,
+    header: "gruepr is flexible",
+    info: "Instructors can include whatever questions they would like to use to help form teams, and the algorithm can use the survey data in a variety of instructor-chosen ways.",
+  },
+  {
+    img: security,
+    header: "gruepr keeps your data private",
+    info: "Instructors can include whatever questions they would like to use to help form teams, and the algorithm can use the survey data in a variety of instructor-chosen ways.",
+  },
 
-       
-    {
-       img: glasses,
-        header: "gruepr was created by an educator for fellow educators",
-        info: "It will remain always free of cost, free from ads, and free from selling of any data."
-
-        
-    },
-   
-
-]
+  {
+    img: glasses,
+    header: "gruepr was created by an educator for fellow educators",
+    info: "It will remain always free of cost, free from ads, and free from selling of any data.",
+  },
+];
---
+++ src/content/content.js
@@ -0,0 +1,110 @@
+export default {
+  emailText: "info@gruepr.com",
+
+  // ----- Navigation bar ----- //
+  navbar: {
+    homeNav: "Home",
+    faqNav: "FAQs",
+    aboutNav: "About Us",
+    downloadButton: "Download",
+  },
+
+  // ----- Footer ----- //
+  footer: {
+    howItWorks: "How It Works",
+    faqNav: "FAQs",
+    makeSurveyNav: "Make a Survey",
+    privacyPolicyNav: "Privacy Policy",
+  },
+
+  // ----- Home page ----- //
+  home: {
+    // Hero
+    heroHeader: "Sort your students into the most optimal grueps",
+    heroText: `Use our survey maker to collect information from your students, upload to gruepr, and our 
+      algorithm will find you the best set of teams.`,
+    downloadGrueprButton: "Download gruepr",
+
+    // Why gruepr
+    grueprFlexibleHeader: "gruepr is flexible",
+    grueprFlexibleText: `Instructors can include whatever questions they would like to use to help form teams, 
+      and the algorithm can use the survey data in a variety of instructor-chosen ways.`,
+
+    grueprPrivateHeader: "gruepr keeps your data private",
+    grueprPrivateText: `Instructors can include whatever questions they would like to use to help form teams, 
+      and the algorithm can use the survey data in a variety of instructor-chosen ways.`,
+
+    grueprEducatorsHeader:
+      "gruepr was created by an educator for fellow educators",
+    grueprEducatorsText:
+      "It will remain always free of cost, free from ads, and free from selling of any data.",
+
+    // Features
+    createSurveyHeader: "Create custom surveys",
+    createSurveyText: `Fill out our form building questionaire to create the perfect survey for your class.
+     Select the types of questions and responses you want to include in your survey and let gruepr do the rest!`,
+
+    teamCompatibilityHeader: "Maximize team compatibility",
+    teamCompatibilityText: `Upload your .csv, select your parameters, and have the gruepings appear right before your eyes!
+      We help to maximize team productivity by pairing students with similar goals and skill sets.`,
+
+    freeOpenSourceHeader: "Free and open source, always",
+    freeOpenSourceText:
+      "Our powerful genetic matching algorithm is available to instructors, completely free and open source.",
+
+    // Trusted
+    trustedHeader: "Trusted by 35+ schools",
+  },
+
+  // ----- FAQ page ----- //
+  faq: {
+    // How gruepr works
+    howGrueprWorks: "How gruepr works",
+    overview: "Overview",
+    surveyMakerOverview: "SurveyMaker Overview",
+    grueprOverview: "gruepr Overview",
+    surveyMakerAttributes: "SurveyMaker Attributes",
+    surveyMakerTimezoneSched: "SurveyMaker Timezone & Schedule",
+
+    // FAQs
+    faqHeader: "FAQ",
+    setNameQuestion: "How do I set the name of each team?",
+    makeSurveyQuestion: "What if I want to make my own survey question(s)?",
+    changeTeamsQuestion:
+      "Can I make changes to the teams after they've been created?",
+    weightedRulesQuestion: "How do the weighted rules work?",
+    attributesQuestion: "What do the Required/Incompatible Attributes mean?",
+
+    // Legal Side
+    legalHeader: "The Legal Side",
+    legalText: `This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+      without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. \n
+      No personal data about or from gruepr's users will ever be intentionally shared. 
+      For more information, we have a complete privacy policy. \n
+      gruepr incorporates code from the open source Qt libraries, using version 5.15. These can be freely downloaded from Qt. \n
+      Icons used in gruepr were originally created by Icons8. 
+      These icons have been made available under the creative commons license: Attribution-NoDerivs 3.0 Unported (CC BY-ND 3.0). \n
+      An embedded font is used in gruepr, mostly for pdf and printer output: 
+      Oxygen Mono, Copyright (C) 2012, Vernon Adams (vern@newtypography.co.uk) released under SIL OPEN FONT LICENSE V1.1. `,
+  },
+
+  // ----- About Us page ----- //
+  aboutUs: {
+    // Backstory
+    backstoryHeader: "The gruepr backstory",
+    backstoryText: `As an educator, I want my students to have the best possible experience during group work. 
+      I created gruepr because I was spending too much time assembling teams that accounted for my students' 
+      individual schedules and past experiences. I wanted a tool that could help me, and I wanted to keep it always 
+      free and open source for my fellow educators. The first version of the app was named "grouper," and that 
+      inspired my scuba-diving colleage to give me a fish logo. The name later changed to "gruepr" because the challenges 
+      of developing my first app reminded me of trying to avoid the fictional monster 
+      that haunts the dark in the first computer game I remember ever playing.`,
+
+    // Contact
+    contactHeader: "Contact",
+    submitIssue: "Locate the issue tracker to submit a new issue here:  ",
+    issueTracker: "Issue Tracker",
+    contactText:
+      "If you find bugs or have ideas about how to improve gruepr, please contact Joshua Hertz at info@gruepr.com.",
+  },
+};
