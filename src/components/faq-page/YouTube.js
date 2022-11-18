d9839bc845fa8a9638f0b9f89d7711bffe93c499
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-18 14:16:34 -0500

    yt embds

--- src/components/faq-page/YouTube.js
+++ src/components/faq-page/YouTube.js
@@ -2,13 +2,51 @@ export const YouTube = () => {
 
 
     return (
-
-        <div className="px-20">
+        <div>
+        <div className="flex flex-row">
+        <div className="px-20 ">
              <h1 class="py-5 text-1xl text-dark font-bold drop-shadow-lg dmsans ">Full App Overview</h1>
             <iframe width="300" height="200" src="https://www.youtube.com/embed/Ja-PF068tII" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
         </div>
+        <div className="px-20">
+             <h1 class="py-5 text-1xl text-dark font-bold drop-shadow-lg dmsans ">Survey Maker Overview</h1>
+            <iframe width="300" height="200" src="https://youtu.be/acS6Kj7gW2A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+        </div>
+        <div className="px-20">
+             <h1 class="py-5 text-1xl text-dark font-bold drop-shadow-lg dmsans ">Gruepr Overview</h1>
+            <iframe width="300" height="200" src="https://www.youtube.com/embed/Ja-PF068tII" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+        </div>
+
+
+
+        <div className="flex flex-row">
+        <div>
+              <div className="px-20">
+             <h1 class="py-5 text-1xl text-dark font-bold drop-shadow-lg dmsans ">SurveyMaker Attributes</h1>
+            <iframe width="300" height="200" src="https://youtu.be/sfaKpslFSbE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div className="px-20">
+             <h1 class="py-5 text-1xl text-dark font-bold drop-shadow-lg dmsans ">SurveyMaker Timezone and Schedule</h1>
+            <iframe width="300" height="200" src="https://youtu.be/kUMC3_nFA20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+
+</div>
 
     )
 
