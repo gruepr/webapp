3e165de324c1f96326bce9250a1925b2c243da42
caocynthia <cynthiacao.work@gmail.com>
2022-11-20 11:51:06 -0500

    edit text styles in WhyGruepr

--- src/components/why-gruepr/WhyGruepr.js
+++ src/components/why-gruepr/WhyGruepr.js
@@ -2,25 +2,19 @@ import { CARD } from "./why-gruepr-card/constants";
 import WhyGrueprCard from "./why-gruepr-card/WhyGrueprCard";
 
 const WhyGruepr = () => {
-    return (
-        <div>
-            <h1 class=" text-center text-3xl text-dark font-bold drop-shadow-lg dmsans pt-10">Why choose Gruepr?</h1>
-          <div class=" ">
-          {CARD.map((whygruepr, index) => (
-                <WhyGrueprCard
-                img={whygruepr.img}
-                header={whygruepr.header}
-                info={whygruepr.info}
-              />
-          ))}
-
-          </div>
-         
-
-        </div>
-
-    )
-
-}
+  return (
+    <div>
+      <h2 class="heading2">Why choose Gruepr?</h2>
+      <div class=" ">
+        {CARD.map((whygruepr, index) => (
+          <WhyGrueprCard
+            img={whygruepr.img}
+            header={whygruepr.header}
+            info={whygruepr.info}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default WhyGruepr;
-
