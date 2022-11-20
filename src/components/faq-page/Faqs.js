7df50b828ea6befac4a707e942e160003c1903e8
caocynthia <cynthiacao.work@gmail.com>
2022-11-20 11:58:40 -0500

    update text styles in faq

--- src/components/faq-page/Faqs.js
+++ src/components/faq-page/Faqs.js
@@ -1,21 +1,20 @@
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
-import Questions from './Questions';
-import Youtube from './YouTube';
+import Questions from "./Questions";
+import Youtube from "./YouTube";
 
 const Faqs = () => {
   return (
     <div>
       <Header />
       <div class="bg-faq-bg">
-        <h1 class=" px-20 py-20 text-3xl text-dark font-bold drop-shadow-lg dmsans ">How Gruepr works</h1>
-        
-        <Youtube/>
-        
-        <Questions/>
-        <Footer/>
-      </div>
+        <h2 class="px-20 py-20 heading2">How Gruepr works</h2>
 
+        <Youtube />
+
+        <Questions />
+        <Footer />
+      </div>
     </div>
   );
 };
