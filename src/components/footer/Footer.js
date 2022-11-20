d2e89e4bd22f4965a352a15f04921d1c7a26905b
caocynthia <cynthiacao.work@gmail.com>
2022-11-20 12:00:08 -0500

    update footer text styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
     <div className="flow-root items-center bg-white px-16 py-1">
       <div className="float-left flex items-center h-20 space-x-10">
         <div className="text-2xl pb-2 paytone text-neutral-500">gruepr</div>
-        <div className="text-base font-bold dmsans text-neutral-500 text-decoration-line: underline space-x-4">
+        <div className="link space-x-4">
           <Link to="/Faqs">How it works</Link>
           <Link to="/Faqs">FAQs</Link>
           <Link to="/Faqs">Privacy Policy</Link>
         </div>
       </div>
       <div className="float-right flex items-center justify-center h-20 space-x-8">
-        <div className="text-base font-normal dmsans text-neutral-500">
+        <div className="body">
           <a onClick={() => console.log("Home")}>info@gruepr.com</a>
         </div>
       </div>
