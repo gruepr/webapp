8396922f54c3bfd293cd384ebf605c00af3f037d
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-21 18:43:25 -0500

    button link for about section

--- src/components/about-us/Contact.js
+++ src/components/about-us/Contact.js
@@ -6,9 +6,8 @@ const Contact = () => {
             <div className="px-20">
             <h1 class="py-10 text-3xl text-dark font-bold drop-shadow-lg dmsans ">Contact</h1>
             <p class=" pb-10 text-gray-700 text-base dmsans dark">Locate the issue tracker to submit a new issue here:  </p>
-            <button className="bg-white py-4 px-10 font-bold dmsans text-black outline">
-                Issue Tracker
-            </button>
+            <a className="bg-white py-4 px-10 font-bold dmsans text-black outline" href="https://github.com/gruepr/gruepr/issues">Issue Tracker</a>
+            
             <p class=" py-10 text-gray-700 text-base dmsans dark">If you find bugs or have ideas about how to improve gruepr, please contact Joshua Hertz at info@gruepr.com.  </p>
             </div>
         </div>
