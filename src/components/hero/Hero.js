9aae7d1f40b91dc415991ba17509fd7974c9bb01
caocynthia <cynthiacao.work@gmail.com>
2022-11-22 03:25:07 -0500

    edit styling in Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,17 +4,15 @@ import content from "../../content/content";
 const Hero = () => {
   return (
     <body>
-      <header className="w-full h-96 bg-home-hero-bg bg-cover bg-center flex justify-center items-center">
-        <div class="flex flex-col justify-center items-center">
-          <h1 class="heading1">{content.home.hero.header}</h1>
-          <p class="heading3">{content.home.hero.text}</p>
-          <button className="secondary-button">
-            <a onClick={() => console.log("FAQs")}>
-              {content.home.hero.button}
-            </a>
-          </button>
-        </div>
-      </header>
+      <div className="w-full h-[38.5rem] bg-home-hero-bg bg-cover bg-center flex flex-col items-center justify-center gap-y-10">
+        <h1 className="heading1 w-[50rem]">{content.home.hero.header}</h1>
+        <p className="heading3 text-center w-[40rem]">
+          {content.home.hero.text}
+        </p>
+        <button className="secondary-button">
+          <a onClick={() => console.log("FAQs")}>{content.home.hero.button}</a>
+        </button>
+      </div>
     </body>
   );
 };
