765ac87aca7038fb33bf8f878c2e5b8ef9a33b1b
caocynthia <cynthiacao.work@gmail.com>
2022-11-22 00:29:43 -0500

    fix extra quotation mark in Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,12 +6,8 @@ const Hero = () => {
     <body>
       <header className="w-full h-96 bg-home-hero-bg bg-cover bg-center flex justify-center items-center">
         <div class="flex flex-col justify-center items-center">
-          <h1 class="heading1">
-            {content.home.hero.header}
-          </h1>
-          <p class="heading3"">
-            {content.home.hero.text}
-          </p>
+          <h1 class="heading1">{content.home.hero.header}</h1>
+          <p class="heading3">{content.home.hero.text}</p>
           <button className="secondary-button">
             <a onClick={() => console.log("FAQs")}>
               {content.home.hero.button}
