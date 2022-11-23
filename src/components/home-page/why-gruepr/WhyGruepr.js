9534a1784026f44f54fbe9223121de9308efb774
caocynthia <cynthiacao.work@gmail.com>
2022-11-23 02:50:08 -0500

    make cards horizontal

--- src/components/home-page/why-gruepr/WhyGruepr.js
+++ src/components/home-page/why-gruepr/WhyGruepr.js
@@ -4,9 +4,9 @@ import content from "../../../content/content";
 
 const WhyGruepr = () => {
   return (
-    <div>
+    <div class="flex flex-col justify-center items-center m-16">
       <h2 class="heading2 text-center">{content.home.whyGruepr.header}</h2>
-      <div class=" ">
+      <div class="grid grid-flow-row sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
         {CARD.map((whygruepr, index) => (
           <WhyGrueprCard
             img={whygruepr.img}
--- src/components/home-page/why-gruepr/why-gruepr-card/WhyGrueprCard.js
+++ src/components/home-page/why-gruepr/why-gruepr-card/WhyGrueprCard.js
@@ -1,18 +1,17 @@
 function WhyGrueprCards(props) {
   return (
-    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      <div class=" bg-accent-50  rounded overflow-hidden shadow-lg">
-        <img
-          class="object-none object-center m-auto"
-          src={props.img}
-          alt="icon"
-        />
-        <div class="px-6 py-4">
-          <h3 class="heading3">{props.header}</h3>
-          <p class="body">{props.info}</p>
-        </div>
+    <div class="bg-accent-50 border-solid border-accent-200 border-2 gap-6 p-8">
+      <img
+        class="object-none object-center m-auto"
+        src={props.img}
+        alt="icon"
+      />
+      <div class="gap-2">
+        <h3 class="heading3 text-center">{props.header}</h3>
+        <p class="body text-center">{props.info}</p>
       </div>
     </div>
+    // </div>
   );
 }
 export default WhyGrueprCards;
