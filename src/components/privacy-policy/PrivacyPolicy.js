ab2ff630e41aad104fa889414334c46125b85b69
caocynthia <cynthiacao.work@gmail.com>
2022-11-29 21:59:40 -0500

    add spacing

--- src/components/privacy-policy/PrivacyPolicy.js
+++ src/components/privacy-policy/PrivacyPolicy.js
@@ -6,9 +6,12 @@ const PrivacyPolicy = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div class="mx-16">
+        <h2 className="heading2 pb-8">{content.privacyPolicy.header}</h2>
         <p className="body">{content.privacyPolicy.beginning}</p>
-        <p className="body">{content.privacyPolicy.linksHeader}</p>
+        <p className="body font-bold pt-6">
+          {content.privacyPolicy.linksHeader}
+        </p>
         <p className="body"> {content.privacyPolicy.linksText} </p>
       </div>
       <Footer />
