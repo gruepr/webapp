99dcfc63f2df7947aac64f3c667f7917423cdefa
caocynthia <cynthiacao.work@gmail.com>
2022-11-29 21:46:11 -0500

    debug privacy policy page

--- src/components/privacy-policy/PrivacyPolicy.js
+++ src/components/privacy-policy/PrivacyPolicy.js
@@ -3,13 +3,9 @@ import content from "../../content/content";
 const PrivacyPolicy = () => {
   return (
     <div>
-      <p class=" px-20 py-20 text-3xl text-dark font-bold drop-shadow-lg dmsans ">
-        {content.privacyPolicy.beginning}
-      </p>
-      <p class=" px-20 pt-20 pb-7 text-3xl text-dark font-bold drop-shadow-lg dmsans ">
-        {content.privacyPolicy.linksHeader}
-      </p>
-      <p class="px-20  text-dark"> {content.privacyPolicy.linksText} </p>
+      <p className="body">{content.privacyPolicy.beginning}</p>
+      <p className="body">{content.privacyPolicy.linksHeader}</p>
+      <p className="body"> {content.privacyPolicy.linksText} </p>
     </div>
   );
 };
