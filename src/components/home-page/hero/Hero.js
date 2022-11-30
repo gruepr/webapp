ff3978e900460660f0dbaa8c00b1ac8a1e8acc04
caocynthia <cynthiacao.work@gmail.com>
2022-11-29 21:00:31 -0500

    add margins

--- src/components/home-page/hero/Hero.js
+++ src/components/home-page/hero/Hero.js
@@ -4,10 +4,10 @@ import content from "../../../content/content";
 const Hero = () => {
   return (
     <div className="w-full h-[38.5rem] bg-hero-bg bg-center flex flex-col items-center justify-center gap-y-10">
-      <h1 className="heading1 lg:w-[50rem] md:w-[30rem]">
+      <h1 className="heading1 ml-12 mr-12 lg:w-[50rem] scale-x">
         {content.home.hero.header}
       </h1>
-      <h3 className="heading3 text-center lg:w-[40rem]">
+      <h3 className="heading3 ml-12 mr-12 text-center lg:w-[40rem] scale-x">
         {content.home.hero.text}
       </h3>
       <button className="secondary-button">
