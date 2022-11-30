e283177578a524e6b781295c42cc5cbe6155ad8a
caocynthia <cynthiacao.work@gmail.com>
2022-11-29 20:44:18 -0500

    add different widths for varying screen sizes

--- src/components/home-page/hero/Hero.js
+++ src/components/home-page/hero/Hero.js
@@ -4,8 +4,10 @@ import content from "../../../content/content";
 const Hero = () => {
   return (
     <div className="w-full h-[38.5rem] bg-hero-bg bg-center flex flex-col items-center justify-center gap-y-10">
-      <h1 className="heading1 w-[50rem]">{content.home.hero.header}</h1>
-      <h3 className="heading3 text-center w-[40rem]">
+      <h1 className="heading1 lg:w-[50rem] md:w-[30rem]">
+        {content.home.hero.header}
+      </h1>
+      <h3 className="heading3 text-center lg:w-[40rem]">
         {content.home.hero.text}
       </h3>
       <button className="secondary-button">
