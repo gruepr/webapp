8faf7b514fb7d971bfbd7f6ea71cfdf900310f00
caocynthia <cynthiacao.work@gmail.com>
2022-11-29 21:09:42 -0500

    fix spacing of grid and change order of image and text depending on size

--- src/components/home-page/features/Features.js
+++ src/components/home-page/features/Features.js
@@ -7,7 +7,8 @@ const Features = () => {
   return (
     <div class="bg-features-bg">
       <div class="flex flex-col justify-center items-center ml-16 mr-16">
-        <div class="mt-20 mb-20 space-x-8 items-center grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+        {/* Create custon surveys */}
+        <div class="mt-20 mb-20 items-center grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
           <div class="object-scale-down">
             <img
               src={survey}
@@ -19,18 +20,20 @@ const Features = () => {
             <p class="body">{content.home.features.createSurveyText}</p>
           </div>
         </div>
-        <div class="mt-20 mb-20 space-x-8 items-center grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-          <div class="resize-x space-y-3">
+        {/* Maximize team compatibility */}
+        <div class="mt-20 mb-20 items-center grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+          <div class="object-scale-down lg:order-last">
+            <img src={team} alt="Gruepr screenshot of team creation screen" />
+          </div>
+          <div class="resize-x space-y-3 lg:order-first">
             <h2 class="heading2">
               {content.home.features.teamCompatibilityHeader}
             </h2>
             <p class="body">{content.home.features.teamCompatibilityText}</p>
           </div>
-          <div class="object-scale-down">
-            <img src={team} alt="Gruepr screenshot of team creation screen" />
-          </div>
         </div>
-        <div class="mt-20 mb-20 space-x-8 items-center grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+        {/* Free and open source, always */}
+        <div class="mt-20 mb-20 items-center grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
           <div class="object-scale-down">
             <img
               src={opensource}
