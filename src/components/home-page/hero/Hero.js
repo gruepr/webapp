619519f7e31fcde568ef07a07d4eb368bb6a58d9
caocynthia <cynthiacao.work@gmail.com>
2022-12-01 15:16:32 -0500

    move text-center to hero header

--- src/components/home-page/hero/Hero.js
+++ src/components/home-page/hero/Hero.js
@@ -3,7 +3,7 @@ import content from "../../../content/content";
 const Hero = () => {
   return (
     <div className="w-full h-[38.5rem] bg-hero-bg bg-center flex flex-col items-center justify-center gap-y-10">
-      <h1 className="heading1 ml-12 mr-12 lg:w-[50rem] scale-x">
+      <h1 className="heading1 text-center ml-12 mr-12 lg:w-[50rem] scale-x">
         {content.home.hero.header}
       </h1>
       <h3 className="heading3 ml-12 mr-12 text-center lg:w-[40rem] scale-x">
