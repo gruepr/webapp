38310106f81a014bf0f0ae6be188c471ecabc69f
caocynthia <cynthiacao.work@gmail.com>
2022-12-01 15:14:06 -0500

    edit contact us styling

--- src/components/about-us/Contact.js
+++ src/components/about-us/Contact.js
@@ -2,21 +2,17 @@ import content from "../../content/content";
 
 const Contact = () => {
   return (
-    <div className="bg-contact-bg ">
-      <div className="px-20">
+    <div className="bg-contact-bg">
+      <div className="space-y-4">
         <h2 class="heading2">{content.aboutUs.contact.header}</h2>
         <p class="body">{content.aboutUs.contact.submitIssue}</p>
-        <a
-          className="secondary-button outline outline-neutral-500"
-          href="https://github.com/gruepr/gruepr-webapp/issues"
-        >
-          {content.aboutUs.contact.issueTracker}
-        </a>
+        <button className="secondary-button outline outline-neutral-500">
+          <a href="https://github.com/gruepr/gruepr-webapp/issues">
+            {content.aboutUs.contact.issueTracker}
+          </a>
+        </button>
 
-        <p class=" py-10 text-gray-700 text-base dmsans dark">
-          If you find bugs or have ideas about how to improve gruepr, please
-          contact Joshua Hertz at info@gruepr.com.{" "}
-        </p>
+        <p class="body">{content.aboutUs.contact.bugsOrImprove}</p>
       </div>
     </div>
   );
