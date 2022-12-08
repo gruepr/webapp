869d370c58ac381459759e36cf367d1b25f65d21
caocynthia <cynthiacao.work@gmail.com>
2022-12-08 17:32:59 -0500

    change trusted schools to content instead of hardcoded text

--- src/components/home-page/trusted-schools/TrustedSchools.js
+++ src/components/home-page/trusted-schools/TrustedSchools.js
@@ -1,12 +1,13 @@
 import Logos from "../../../img/Logos.svg";
+import content from "../../../content/content";
 
 export const TrustedSchools = () => {
   return (
     <div class="bg-primary-50 laptop:p-12 laptop:space-y-8 mobile:p-[16px] mobile:space-y-[16px]">
       <h2 class="heading2 laptop:text-center mobile:text-left">
-        Trusted By 35+ Schools
+        {content.home.trustedHeader}
       </h2>
-      <img class="object-fill" src={Logos} alt="logos" />
+      <img class="object-cover w-full" src={Logos} alt="logos" />
     </div>
   );
 };
