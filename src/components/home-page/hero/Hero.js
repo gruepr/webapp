cfdbdbcc259053d5617502b7a39d36935ce58a74
caocynthia <cynthiacao.work@gmail.com>
2022-12-08 17:28:52 -0500

    fix spacing in hero

--- src/components/home-page/hero/Hero.js
+++ src/components/home-page/hero/Hero.js
@@ -5,15 +5,18 @@ const Hero = () => {
     <div
       className="bg-hero-bg bg-cover bg-center flex flex-col items-center justify-center 
     laptop:gap-y-10 laptop:py-[120px]
-    mobile:py-[32px]"
+    mobile:py-[32px] mobile:space-y-[32px]"
     >
       <h1
         className="text-center scale-x laptop:heading1 laptop:px-12 lg:w-[50rem]
-      mobile:mobile-heading"
+      mobile:mobile-heading mobile:px-[16px]"
       >
         {content.home.hero.header}
       </h1>
-      <h3 className="text-center scale-x laptop:heading3 laptop:px-12 laptop:w-[40rem]">
+      <h3
+        className="text-center scale-x heading3 laptop:px-12 laptop:w-[40rem]
+      mobile:px-[16px]"
+      >
         {content.home.hero.text}
       </h3>
       <button className="secondary-button">
