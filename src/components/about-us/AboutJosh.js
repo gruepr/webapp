363d377ffd6acfc5c0d0291fed48e96cb8f51421
caocynthia <cynthiacao.work@gmail.com>
2022-12-08 16:52:11 -0500

    edit styling in about josh

--- src/components/about-us/AboutJosh.js
+++ src/components/about-us/AboutJosh.js
@@ -3,7 +3,7 @@ import content from "../../content/content";
 
 const AboutJosh = () => {
   return (
-    <div className="laptop:bg-primary-50 laptop:py-10 laptop:px-20 tablet:bg-primary-50 mobile:p-4 mobile:bg-white">
+    <div className="laptop:bg-primary-50 laptop:py-10 laptop:px-20 tablet:bg-primary-50 tablet:p-8 mobile:p-4 mobile:bg-white">
       <div
         className="flex py-2 items-center 
       laptop:flex-row laptop:space-x-8
