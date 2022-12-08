87e3280c9cb4765d503a2b4ff2b2f2d750ef3724
caocynthia <cynthiacao.work@gmail.com>
2022-12-08 16:52:17 -0500

    make contact page responsive

--- src/components/about-us/Contact.js
+++ src/components/about-us/Contact.js
@@ -3,18 +3,27 @@ import DecorativeImage from "../../img/contact-bottom.svg";
 
 const Contact = () => {
   return (
-    <div className="bg-contact-bg bg-[center_top_1rem]">
-      <div className="space-y-4 p-16 pb-8">
+    <div className="laptop:bg-cover laptop:bg-contact-bg laptop:bg-[right_top_1rem] mobile:bg-cover mobile:bg-[right_top]">
+      <div
+        className="laptop:space-y-4 laptop:p-16 laptop:pb-8
+      mobile:space-y-3 mobile:p-3 mobile:pt-10"
+      >
         <h2 class="heading2">{content.aboutUs.contact.header}</h2>
-        <p class="body">{content.aboutUs.contact.submitIssue}</p>
-        <button className="secondary-button outline outline-neutral-500">
-          <a target="_blank" href="https://github.com/gruepr/gruepr/issues">
-            {content.aboutUs.contact.issueTracker}
-          </a>
-        </button>
-        <p class="body">{content.aboutUs.contact.bugsOrImprove}</p>
+        <div className="laptop:space-y-4 mobile:space-y-5">
+          <p class="body">{content.aboutUs.contact.submitIssue}</p>
+          <button className="secondary-button outline outline-neutral-500">
+            <a target="_blank" href="https://github.com/gruepr/gruepr/issues">
+              {content.aboutUs.contact.issueTracker}
+            </a>
+          </button>
+          <p class="body">{content.aboutUs.contact.bugsOrImprove}</p>
+        </div>
       </div>
-      <img src={DecorativeImage} alt="decorative image" />
+      <img
+        class="object-cover w-full"
+        src={DecorativeImage}
+        alt="decorative image"
+      />
     </div>
   );
 };
