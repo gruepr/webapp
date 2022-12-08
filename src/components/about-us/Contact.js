27fb9cbd7eeb2b6ea687db22c5031f17e5d1d047
caocynthia <cynthiacao.work@gmail.com>
2022-12-08 16:31:16 -0500

    edit styling in contact

--- src/components/about-us/Contact.js
+++ src/components/about-us/Contact.js
@@ -1,19 +1,20 @@
 import content from "../../content/content";
+import DecorativeImage from "../../img/contact-bottom.svg";
 
 const Contact = () => {
   return (
-    <div className="bg-contact-bg">
-      <div className="space-y-4">
+    <div className="bg-contact-bg bg-[center_top_1rem]">
+      <div className="space-y-4 p-16 pb-8">
         <h2 class="heading2">{content.aboutUs.contact.header}</h2>
         <p class="body">{content.aboutUs.contact.submitIssue}</p>
         <button className="secondary-button outline outline-neutral-500">
-          <a href="https://github.com/gruepr/gruepr-webapp/issues">
+          <a target="_blank" href="https://github.com/gruepr/gruepr/issues">
             {content.aboutUs.contact.issueTracker}
           </a>
         </button>
-
         <p class="body">{content.aboutUs.contact.bugsOrImprove}</p>
       </div>
+      <img src={DecorativeImage} alt="decorative image" />
     </div>
   );
 };
