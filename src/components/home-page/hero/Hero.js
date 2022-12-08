9dc8b847815bfcde1a99d0075002be4eb5ef00d2
caocynthia <cynthiacao.work@gmail.com>
2022-12-08 17:32:46 -0500

    edit text spacing in hero

--- src/components/home-page/hero/Hero.js
+++ src/components/home-page/hero/Hero.js
@@ -4,18 +4,22 @@ const Hero = () => {
   return (
     <div
       className="bg-hero-bg bg-cover bg-center flex flex-col items-center justify-center 
-    laptop:gap-y-10 laptop:py-[120px]
-    mobile:py-[32px] mobile:space-y-[32px]"
+      laptop:gap-y-10 laptop:py-[120px]
+      mobile:py-[32px] mobile:space-y-[32px]"
     >
       <h1
-        className="text-center scale-x laptop:heading1 laptop:px-12 lg:w-[50rem]
-      mobile:mobile-heading mobile:px-[16px]"
+        className="text-center scale-x laptop:heading1 
+        laptop:px-12 laptop:w-[50rem]
+        tablet:w-[40rem]
+        mobile:mobile-heading mobile:px-[16px]"
       >
         {content.home.hero.header}
       </h1>
       <h3
-        className="text-center scale-x heading3 laptop:px-12 laptop:w-[40rem]
-      mobile:px-[16px]"
+        className="text-center scale-x heading3 
+        laptop:px-12 laptop:w-[40rem]
+        tablet:w-[40rem]
+        mobile:px-[16px]"
       >
         {content.home.hero.text}
       </h3>
