57dc084791590fe056091eb3ce0129cfa5465bff
caocynthia <cynthiacao.work@gmail.com>
2022-12-13 14:40:45 -0500

    edit spacing

--- src/components/about-us/AboutJosh.js
+++ src/components/about-us/AboutJosh.js
@@ -6,8 +6,8 @@ const AboutJosh = () => {
     <div className="laptop:bg-primary-50 laptop:py-10 laptop:px-20 tablet:bg-primary-50 tablet:p-8 mobile:p-4 mobile:bg-white">
       <div
         className="flex py-2 items-center 
-      laptop:flex-row laptop:space-x-8
-      mobile:flex-col mobile:space-x-0 mobile:space-y-3"
+        laptop:flex-row laptop:space-x-8
+        mobile:flex-col mobile:space-x-0 mobile:space-y-3"
       >
         <img src={joshImage} className="h-[19rem] mobile:h-[18.75rem]" />
         <div className="flex flex-col space-y-2">
