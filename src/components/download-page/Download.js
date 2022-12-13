30f0969fed582346b080fb67e1aced30895090c0
caocynthia <cynthiacao.work@gmail.com>
2022-12-12 22:33:55 -0500

    style content

---
+++ src/components/download-page/Download.js
@@ -0,0 +1,15 @@
+import content from "../../../content/content";
+
+const Download = () => {
+  return (
+    <div className="">
+      <h2 className="heading2">{content.downloadPage.header}</h2>
+      <h3 className="heading3">{content.downloadPage.windowsInstall}</h3>
+      <h3 className="heading3">{content.downloadPage.macInstall}</h3>
+      <button className="secondary-button">
+        <a href="">{content.home.hero.button}</a>
+      </button>
+    </div>
+  );
+};
+export default Download;
