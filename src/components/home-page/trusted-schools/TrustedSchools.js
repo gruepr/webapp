a3e6befa8dc0761a7b5446b284ce37009b7d0528
caocynthia <cynthiacao.work@gmail.com>
2022-12-18 15:37:38 -0500

    replace class with classname

--- src/components/home-page/trusted-schools/TrustedSchools.js
+++ src/components/home-page/trusted-schools/TrustedSchools.js
@@ -3,12 +3,12 @@ import content from "../../../content/content";
 
 export const TrustedSchools = () => {
   return (
-    <div class="bg-primary-50 laptop:p-12 laptop:space-y-8 tablet:p-12 mobile:p-[16px] mobile:space-y-[16px]">
-      <h2 class="heading2 laptop:text-center tablet:text-center mobile:text-left">
+    <div className="bg-primary-50 laptop:p-12 laptop:space-y-8 tablet:p-12 mobile:p-[16px] mobile:space-y-[16px]">
+      <h2 className="heading2 laptop:text-center tablet:text-center mobile:text-left">
         {content.home.trustedHeader}
       </h2>
       <img
-        class="object-cover w-full"
+        className="object-cover w-full"
         src={Logos}
         alt="School logos of The University of Tennessee, Santa Clara University, Rowan University, 
         Ohio State University, Georgetown University, University of California Irvine, Monash University, Indiana University Bloomington,
