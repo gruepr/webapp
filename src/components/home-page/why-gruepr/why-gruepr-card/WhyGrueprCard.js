0ff8c82861c36639430148f86286cac9d778f347
caocynthia <cynthiacao.work@gmail.com>
2022-12-18 15:01:32 -0500

    replace class with className

--- src/components/home-page/why-gruepr/why-gruepr-card/WhyGrueprCard.js
+++ src/components/home-page/why-gruepr/why-gruepr-card/WhyGrueprCard.js
@@ -1,18 +1,18 @@
 function WhyGrueprCards(props) {
   return (
     <div
-      class="bg-accent-50 border-solid border-accent-200 border-2 
+      className="bg-accent-50 border-solid border-accent-200 border-2 
     laptop:p-7 laptop:py-8 laptop:space-y-4
     mobile:p-[16px]"
     >
       <img
-        class="object-none object-center m-auto"
+        className="object-none object-center m-auto"
         src={props.img}
         alt={props.alt}
       />
-      <div class="space-y-2">
-        <h3 class="heading3 text-center">{props.header}</h3>
-        <p class="body text-center">{props.info}</p>
+      <div className="space-y-2">
+        <h3 className="heading3 text-center">{props.header}</h3>
+        <p className="body text-center">{props.info}</p>
       </div>
     </div>
   );
